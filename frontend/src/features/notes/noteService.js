6e6e4ae9802554385692ac40a820ae2f199196e2
jquick5 <jay.quick79@gmail.com>
2022-09-14 15:01:50 -0700

    Added note system to fronend ready for deployment

---
+++ frontend/src/features/notes/noteService.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const API_URL = '/api/tickets/';
+
+// Get ticket notes
+const getNotes = async (ticketId, token) => {
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	const res = await axios.get(API_URL + ticketId + '/notes', config);
+
+	return res.data;
+};
+
+// Add ticket note
+const createNote = async (noteText, ticketId, token) => {
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	const res = await axios.post(
+		API_URL + ticketId + '/notes',
+		{ text: noteText },
+		config
+	);
+
+	return res.data;
+};
+
+const noteService = {
+	getNotes,
+	createNote,
+};
+
+export default noteService;
--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,14 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice';
+import { getTicket, closeTicket } from '../features/tickets/ticketSlice';
+import {
+	getNotes,
+	reset as notesReset,
+	createNote,
+} from '../features/notes/noteSlice';
 import { useParams, useNavigate } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
+import Modal from 'react-modal';
+import { FaPlus } from 'react-icons/fa';
+import NoteItem from '../components/NoteItem';
+
+const customStyles = {
+	content: {
+		width: '600px',
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)',
+		position: 'relative',
+	},
+};
+
+Modal.setAppElement('#root');
 
 const Ticket = () => {
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [noteText, setNoteText] = useState('');
 	const { ticket, isLoading, isSuccess, isError, message } = useSelector(
 		(state) => state.tickets
+	);
+	const { notes, isLoading: notesIsLoading } = useSelector(
+		(state) => state.notes
 	);
 
 	const params = useParams();
@@ -22,6 +50,7 @@ const Ticket = () => {
 		}
 
 		dispatch(getTicket(ticketId));
+		dispatch(getNotes(ticketId));
 		// eslint-disable-next-line
 	}, [isError, message, ticketId]);
 
@@ -31,7 +60,16 @@ const Ticket = () => {
 		navigate('/tickets');
 	};
 
-	if (isLoading) {
+	const openModal = () => setModalIsOpen(true);
+	const closeModal = () => setModalIsOpen(false);
+
+	const onNoteSubmit = (e) => {
+		e.preventDefault();
+		dispatch(createNote({ noteText, ticketId }));
+		closeModal();
+	};
+
+	if (isLoading || notesIsLoading) {
 		return <Spinner />;
 	}
 	if (isError) {
@@ -56,7 +94,47 @@ const Ticket = () => {
 					<h3>Description of issue</h3>
 					<p>{ticket.description}</p>
 				</div>
+				<h2>Notes</h2>
 			</header>
+			{ticket.status !== 'closed' && (
+				<button className='btn' onClick={openModal}>
+					<FaPlus />
+					Add Note
+				</button>
+			)}
+
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={closeModal}
+				style={customStyles}
+				contentLabel='Add Note'
+			>
+				<h2>Add Note</h2>
+				<button className='btn-close' onClick={closeModal}>
+					X
+				</button>
+				<form onSubmit={onNoteSubmit}>
+					<div className='form-group'>
+						<textarea
+							name='noteText'
+							id='noteText'
+							className='form-control'
+							value={noteText}
+							placeholder='Note Text'
+							onChange={(e) => setNoteText(e.target.value)}
+						></textarea>
+					</div>
+					<div className='form-group'>
+						<button className='btn' type='submit'>
+							Submit
+						</button>
+					</div>
+				</form>
+			</Modal>
+
+			{notes.map((note) => (
+				<NoteItem key={note._id} note={note} />
+			))}
 			{ticket.status !== 'closed' && (
 				<button onClick={onTicketClose} className='btn btn-block btn-danger'>
 					Close Ticket
